cff423463acfb4438e9b5b5f1fa56e48ad04ad8c
Brayan Torres <47356178+BrayanTorres2@users.noreply.github.com>
2022-06-22 18:05:25 -0500

    Update decorador_definicion.py

--- code/decorador_definicion.py
+++ code/decorador_definicion.py
@@ -16,3 +16,15 @@ saludo()                   # La función saludo está ahora decorada
 # Salida:
 # Esto se añade a mi función original.
 # ¡Hola!
+
+
+
+#------------------------------------------------------------------------ otra forma de hacerlo
+@decorador# azucar de sintaxis, o azucar sintactica 
+def saludo():
+    print("¡Hola!")
+    
+saludo()
+# Salida:
+# Esto se añade a mi función original.
+# ¡Hola!
